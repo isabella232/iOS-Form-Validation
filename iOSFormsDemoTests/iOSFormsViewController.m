9789b0a35fe07cad5a76b4d8ad364885f4910cb1
abarrell <abarrell@umich.edu>
2015-03-11 01:14:41 -0400

    added image to project folder

--- iOSFormsDemoTests/iOSFormsViewController.m
+++ iOSFormsDemoTests/iOSFormsViewController.m
@@ -8,7 +8,8 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
-#include "ViewController.h"
+#import "ViewController.h"
+#import "AppDelegate.h"
 
 @interface iOSFormsViewController : XCTestCase
 
@@ -32,30 +33,45 @@
 }
 
 - (void)testLabelsOnStartup {
-    ViewController *testController;
+    app = [UIApplication sharedApplication];
+    ViewController view = (ViewController*)[[[UIApplication sharedApplication] delegate]];
+    view           = ViewController.view;
     NSString *stdLabel = @"This field is required";
-    XCTAssert(![testController.emailReq.text isEqualToString:stdLabel]);
-    XCTAssert(![testController.nameReq.text isEqualToString:stdLabel]);
-    XCTAssert(![testController.dateReq.text isEqualToString:stdLabel]);
-    XCTAssert(![testController.dateReq isHidden]);
-    XCTAssert(![testController.nameReq isHidden]);
-    XCTAssert(![testController.emailReq isHidden]);
+    XCTAssert([testController.emailReq.text isEqualToString:stdLabel]);
+    XCTAssert([testController.nameReq.text isEqualToString:stdLabel]);
+    XCTAssert([testController.dateReq.text isEqualToString:stdLabel]);
+    XCTAssert([testController.dateReq isHidden]);
+    XCTAssert([testController.nameReq isHidden]);
+    XCTAssert([testController.emailReq isHidden]);
 }
 
 -(void)testAccessibilityLabelsAndHintsOnStartup {
     ViewController *testController;
     NSString *stdHint = @"This field is required";
-    XCTAssert(![testController.emailField.accessibilityHint isEqualToString:stdHint]);
-    XCTAssert(![testController.nameField.accessibilityHint isEqualToString:stdHint]);
-    XCTAssert(![testController.dateField.accessibilityHint isEqualToString:stdHint]);
-    XCTAssert(![testController.dateField.accessibilityLabel isEqualToString:@"Date"]);
-    XCTAssert(![testController.nameField.accessibilityLabel isEqualToString:@"Name"]);
-    XCTAssert(![testController.emailField.accessibilityLabel isEqualToString:@"Email"]);
+    XCTAssert([testController.emailField.accessibilityHint isEqualToString:stdHint]);
+    XCTAssert([testController.nameField.accessibilityHint isEqualToString:stdHint]);
+    XCTAssert([testController.dateField.accessibilityHint isEqualToString:stdHint]);
+    XCTAssert([testController.dateField.accessibilityLabel isEqualToString:@"Date"]);
+    XCTAssert([testController.nameField.accessibilityLabel isEqualToString:@"Name"]);
+    XCTAssert([testController.emailField.accessibilityLabel isEqualToString:@"Email"]);
 }
 
 - (void)testRegex {
-   // ViewController *testController;
-    //testController.emailField.text = @"";
+    NSString *stdLabel =@"This field is required";
+    ViewController *testController;
+    
+    
+    testController.emailField.text = @"";
+    testController.nameField.text = @"";
+    testController.dateField.text = @"";
+    XCTAssert([testController.emailReq.text isEqualToString:stdLabel]);
+    XCTAssert(![testController.emailReq isHidden]);
+    XCTAssert([testController.dateReq.text isEqualToString:stdLabel]);
+    XCTAssert(![testController.dateReq isHidden]);
+    XCTAssert([testController.nameReq.text isEqualToString:stdLabel]);
+    XCTAssert(![testController.nameReq isHidden]);
+
+    
     return;
 }
 
